560d400cd9951c0da53ee67cc5a5cfdf6b8e6472
FizzyMcPhysics <benjaminconnell@gmail.com>
2014-04-01 20:54:36 +0100

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,3 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.Eysys;
+
 import static java.lang.Math.round;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,8 +41,7 @@ public class Problem2 {
        System.out.println("The range of the array is " + range + ",");
        System.out.println("with numbers ranging from " + first + " to " + last + ".");
        System.out.println();
-       
-       
+              
        double meanAverageOfArray = findMeanAverage(array, arraySize);
        System.out.println("The mean average of the array elements is " + meanAverageOfArray + ".");
        System.out.println();
@@ -43,7 +50,7 @@ public class Problem2 {
        System.out.println("The median average of the array elements is " + medianAverageOfArray + ".");
        System.out.println();
        
-       ArrayList modeAverageOfArray = findModeAverage(array, arraySize, range);
+       ArrayList modeAverageOfArray = findModeAverage(array, arraySize, last);
        System.out.println("The mode average of the array elements is/are " + modeAverageOfArray + ".");
        System.out.println();
        
@@ -58,7 +65,7 @@ public class Problem2 {
         
         int arraySize = 50 + randomNumber.nextInt(25);
         int arrayRange = 40 + randomNumber.nextInt(20);
-        System.out.println("range: " + arrayRange + " size: " + arraySize);
+        //System.out.println("range: " + arrayRange + " size: " + arraySize);
         int[] someHugeArray = new int[arraySize]; 
         
         for (int i=0; i<arraySize; i++) {
@@ -95,31 +102,27 @@ public class Problem2 {
         return medianAverage;
     }
     
-    private static ArrayList findModeAverage(int[] array, int size, int range) {
+    private static ArrayList findModeAverage(int[] array, int size, int highestNumber) {
         ArrayList maxKey = new ArrayList();
         int maxCounts = 0;
-        int[] counts = new int[size+1];
-        
+        int[] counts = new int[highestNumber+1];
         for (int i=0; i<size; i++) {            
             counts[array[i]]++;
             
-            if (maxCounts == counts[array[i]]) {
-                
-                int index = array[i];
-                String newWinner = Integer.toString(index);
-                
-                maxKey.add(newWinner);
-            }
             if (maxCounts < counts[array[i]]) {
                 
                 maxCounts = counts[array[i]];
                 maxKey.clear();
+         
+            }
+            if (maxCounts <= counts[array[i]]) {
                 
                 int index = array[i];
                 String newWinner = Integer.toString(index);
                 
                 maxKey.add(newWinner);
             }
+            
         }
         return maxKey;
     }
